3333e9cf10c9fc068a56fc80593647f44dd49378
Iamneo <techadmin@iamneo.ai>
2023-08-02 09:58:14 +0000

    Backup Commit

---
+++ reactapp/src/components/UI/Button/Button.js
@@ -0,0 +1,11 @@
+Button.js
+
+import React from 'react'
+
+const Button = ({children, onClick, disabled}) => {
+  return (
+    <button onClick = {onClick} disabled = {disabled}>{children}</button>
+  )
+}
+
+export default Button
