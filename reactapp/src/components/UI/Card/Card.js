c3b0b5cbad75b6ae0bd049b8df179482c27513a2
Iamneo <techadmin@iamneo.ai>
2023-08-04 12:43:33 +0000

    Backup Commit

--- reactapp/src/components/UI/Card/Card.js
+++ reactapp/src/components/UI/Card/Card.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Button from '../Button/Button';
+let count=0;
 const Card = ({key,
     question,
     correctAnswerMarkUpdate,
@@ -7,8 +8,12 @@ const Card = ({key,
     options,
     answer}) => {
         const [state, setSate] = useState(false);
-        function disableBtn()
+        function disableBtn(selectedOptions)
         {
+            if(selectedOptions===answer){
+                count=count+1;
+                console.log(count);
+            }
             setSate(true);
         }
 
@@ -18,10 +23,11 @@ const Card = ({key,
             <h4>
                 {question}
             </h4>
-            <Button onClick={disableBtn} disabled = {state} >{options.option1}</Button>
-            <Button onClick={disableBtn} disabled = {state} >{options.option2}</Button>
-            <Button onClick={disableBtn} disabled = {state} >{options.option3}</Button>
-            <Button onClick={disableBtn} disabled = {state} >{options.option4}</Button>
+            <Button onClick={()=>disableBtn(options.option1)} disabled = {state} >{options.option1}</Button>
+            <Button onClick={()=>disableBtn(options.option2)} disabled = {state} >{options.option2}</Button>
+            <Button onClick={()=>disableBtn(options.option3)} disabled = {state} >{options.option3}</Button>
+            <Button onClick={()=>disableBtn(options.option4)} disabled = {state} >{options.option4}</Button>
+            {(key===5 && state==='true')?<Button></Button>:""}
             </div>
         )
     }
