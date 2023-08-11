3c81f26f2a226a3285e196532fc8893b20f55c51
Iamneo <techadmin@iamneo.ai>
2023-08-11 18:52:09 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,125 +1,43 @@
-
 import './App.css';
 import Button from "./components/UI/Button/Button";
 import Card from "./components/UI/Card/Card";
-import { useState,React } from 'react';
+import { useState, React } from 'react';
+
 function App() {
+    const questionData = [
+        // ... (your question data remains unchanged)
+    ];
 
-  var qCards;
-  var startBtn;
-  //var resultBtn;
+    const [startFlag, setStartFlag] = useState(false);
 
-  const questionData = [
-    {
-      key:1,
-      question:'Who are the father of the nation ?',
-      //correctAnswerMarkUpdate,
-      //attempt,
-      options : {
-        option1: 'Mahatma Gandhi',
-        option2: 'Jawaharlal Nehru',
-        option3: 'Donald Trump',
-        option4: 'Barrack Obama'
-      },
-      answer : 'Mahatma Gandhi'
-    },
-    {
-      key : 2,
-      question : "What colors is are the leaves ?",
-      //correctAnswerMarkUpdate,
-      //attempt,
-      options : {
-        option1 : "Blue",
-        option2 : "Red",
-        option3 : "Yellow",
-        option4 : "Green",
-      },
-      answer : "Green"
-    },
-    {
-      key:3,
-      question:'Who is the fathers of the nation ?',
-      //correctAnswerMarkUpdate,
-      //attempt,
-      options : {
-        option1: 'Mahatma Gandhi',
-        option2: 'Jawaharlal Nehru',
-        option3: 'Donald Trump',
-        option4: 'Barrack Obama'
-      },
-      answer : 'Mahatma Gandhi'
-    },
-    {
-      key:4,
-      question:'Who is the fathers of the nation ?',
-      //correctAnswerMarkUpdate,
-      //attempt,
-      options : {
-        option1: 'Mahatma Gandhi',
-        option2: 'Jawaharlal Nehru',
-        option3: 'Donald Trump',
-        option4: 'Barrack Obama'
-      },
-      answer : 'Mahatma Gandhi'
-    },
-    {
-      key:5,
-      question:'Who is the father of the nation ?',
-      //correctAnswerMarkUpdate,
-      //attempt,
-      options : {
-        option1: 'Mahatma Gandhi',
-        option2: 'Jawaharlal Nehru',
-        option3: 'Donald Trump',
-        option4: 'Barrack Obama'
-      },
-      answer : 'Mahatma Gandhi'
-    },
+    function handleStart() {
+        setStartFlag(true);
+    }
 
-  ]
-  const [startFlag, setStartFlag] = useState(false);
-  //const [resultFlag, setResultFlag] = useState(false);
-  //const [buttonText, setButtonText] = useState('Start Quiz');
-  //const [questionsCorrect, setQuestionsCorrect] = useState(0);
-  //const [answered, setAnswered] = useState(0);
+    let qCards = null;
+    if (startFlag) {
+        qCards = questionData.map((data) => (
+            <Card
+                key={data.key}
+                question={data.question}
+                options={{
+                    option1: data.options.option1,
+                    option2: data.options.option2,
+                    option3: data.options.option3,
+                    option4: data.options.option4,
+                }}
+            ></Card>
+        ));
+    }
 
-  function handleStart()
-  {
-    setStartFlag(true);
-    //setButtonText('Started');
-  }
-
-  if(startFlag)
-  {
-    qCards = 
-    <div>
-      {
-        questionData.map((data) => 
-          <Card question= {data.question} options = {{
-            option1 : data.options.option1,
-            option2 : data.options.option2,
-            option3 : data.options.option3,
-            option4 : data.options.option4,
-          }} > </Card>)
-      }
-    </div> 
-
-    startBtn = <></>;
-  }
-  else
-  {
-    startBtn = <Button onClick = {handleStart}>Start Quiz</Button>;
-  }
-  
-  return (
-    <div className="App">
-      <h1>
-        Quizz App
-      </h1>
-      {qCards}
-      {startBtn}
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Quizz App</h1>
+            {qCards}
+            {!startFlag && <Button onClick={handleStart}>Start Quiz</Button>}
+            {startFlag && <Button>Show Results</Button>}
+        </div>
+    );
 }
 
 export default App;
