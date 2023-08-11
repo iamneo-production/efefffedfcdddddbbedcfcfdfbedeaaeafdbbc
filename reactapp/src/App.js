26204ce51c4112b34a6299e1585fcb495cff6f10
Iamneo <techadmin@iamneo.ai>
2023-08-11 18:57:09 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,17 +1,78 @@
 import './App.css';
 import Button from "./components/UI/Button/Button";
 import Card from "./components/UI/Card/Card";
-import { useState, React } from 'react';
+import { useState } from 'react';
 
 function App() {
     const questionData = [
-        // ... (your question data remains unchanged)
+        {
+            key: 1,
+            question: 'Who are the father of the nation?',
+            options: {
+                option1: 'Mahatma Gandhi',
+                option2: 'Jawaharlal Nehru',
+                option3: 'Donald Trump',
+                option4: 'Barrack Obama'
+            },
+            answer: 'Mahatma Gandhi'
+        },
+        {
+            key: 2,
+            question: 'What color are the leaves?',
+            options: {
+                option1: 'Blue',
+                option2: 'Red',
+                option3: 'Yellow',
+                option4: 'Green'
+            },
+            answer: 'Green'
+        },
+        {
+            key: 3,
+            question: 'Who is the father of the nation?',
+            options: {
+                option1: 'Mahatma Gandhi',
+                option2: 'Jawaharlal Nehru',
+                option3: 'Donald Trump',
+                option4: 'Barrack Obama'
+            },
+            answer: 'Mahatma Gandhi'
+        },
+        {
+            key: 4,
+            question: 'Who is the father of the nation?',
+            options: {
+                option1: 'Mahatma Gandhi',
+                option2: 'Jawaharlal Nehru',
+                option3: 'Donald Trump',
+                option4: 'Barrack Obama'
+            },
+            answer: 'Mahatma Gandhi'
+        },
+        {
+            key: 5,
+            question: 'Who is the father of the nation?',
+            options: {
+                option1: 'Mahatma Gandhi',
+                option2: 'Jawaharlal Nehru',
+                option3: 'Donald Trump',
+                option4: 'Barrack Obama'
+            },
+            answer: 'Mahatma Gandhi'
+        }
     ];
 
     const [startFlag, setStartFlag] = useState(false);
+    const [resetFlag, setResetFlag] = useState(false);
 
     function handleStart() {
         setStartFlag(true);
+        setResetFlag(false);
+    }
+
+    function handleReset() {
+        setStartFlag(false);
+        setResetFlag(true);
     }
 
     let qCards = null;
@@ -35,7 +96,8 @@ function App() {
             <h1>Quizz App</h1>
             {qCards}
             {!startFlag && <Button onClick={handleStart}>Start Quiz</Button>}
-            {startFlag && <Button>Show Results</Button>}
+            {startFlag && !resetFlag && <Button>Show Results</Button>}
+            {startFlag && resetFlag && <Button onClick={handleReset}>Start Quiz</Button>}
         </div>
     );
 }
